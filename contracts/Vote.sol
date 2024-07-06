6636e2cff6e08ec061f1e34c7904da22d26a3a93
Varun Singh <varunsingh9114@gmail.com>
2024-07-06 23:36:16 +0530

    vote: Rewrite contract to suite use-case.

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -2,28 +2,49 @@
 
 pragma solidity ^0.8.24;
 
-contract Vote{
-    uint public x;
-    uint public y;
+struct Candidate {
+    address addr;
+    string  name;
+    uint128 vote;
+}
+
+
+contract Vote {
+
+    uint128 public voter_count;
+    mapping (uint128 => address) public voter_list;
+    uint128 public candidate_count;
+    mapping (uint128 => Candidate) public candidate_list;
+
 
     constructor(){
-        x = 0;
-        y = 0;
+        candidate_count = 0;
+        voter_count = 0;
     }
 
-    function voteFirst() public{
-        x+=1;
+    function register_voter(address voter) public returns(bool){
+        voter_list[voter_count++] = voter;
+        return true;
     }
 
-    function voteSecond() public {
-        y+=1;
+    function get_voter_count() public view returns(uint128){
+        return voter_count;
     }
 
-    function getVoteFirst() public view returns(uint){
-        return x;
+    function register_candidate(address addr, string memory name) public returns(uint128){
+        candidate_list[candidate_count++] = Candidate(addr, name, 0);
+
+        return candidate_count;
     }
 
-    function getVoteSecond() public view returns(uint){
-        return y;
+    function get_candidate_count() public view returns(uint128){
+        return candidate_count;
+    }
+
+    function vote(uint128 cand_id) public returns(string memory){
+        Candidate storage cand = candidate_list[cand_id-1]; // for 1st candidate the index is 0
+        cand.vote += 1;
+        string memory name = cand.name;
+        return name;
     }
 }
